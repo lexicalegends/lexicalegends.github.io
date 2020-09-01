6087b01f7cf8a532e525c6a5fa19a2d460be14fe
united-geekdom <kirannithilan@gmail.com>
2020-09-01 15:09:15 +0530

    Add jumbled sentence

---
+++ games/jumbled-sentence/reader.js
@@ -0,0 +1,15 @@
+var data = location.href;
+data = data.split("#//");
+data.shift();
+var counter = 3;
+var table = "";
+document.getElementById('heading').innerHTML = data[0];
+for (var i = 0; i < data[1]; i++) {
+  table += "<tr>";
+  for (var j = 0; j < data[2]; j++) {
+    table += "<td>" + "<a onclick='check(" + data[counter] + ")' id='" + data[counter] + "'>" + data[counter + 1] + "</a></td>";
+    counter+= 2;
+  }
+  table += "</tr>"
+}
+document.getElementById('table').innerHTML = table;
---
+++ games/jumbled-sentence/writer.js
@@ -0,0 +1,36 @@
+var link = "game.html";
+var content;
+var order;
+function tableContent() {
+  link += "#//" + document.getElementById('title').value;
+  link += "#//" + document.getElementById('collumn').value;
+  link += "#//" + document.getElementById('row').value;
+  var table1 = "";
+  for (var i = 0; i < document.getElementById('collumn').value; i++) {
+    table1 += "<tr>";
+    for (var j = 0; j < document.getElementById('row').value; j++) {
+      table1 += "<td><input value='' class='content'></td>";
+    }
+    table1 += "</tr>"
+  }
+  document.getElementById('contentTable').innerHTML = table1 + '    <button type="button" onclick="tableOrder()">Go!</button>';
+}
+function tableOrder() {
+  var table3 = "";
+  for (var i = 0; i < document.getElementById('collumn').value; i++) {
+    table3 += "<tr>";
+    for (var j = 0; j < document.getElementById('row').value; j++) {
+      table3 += "<td><input type='number' min='-1' max='100' class='order'></td>";
+    }
+    table3 += "</tr>"
+  }
+  document.getElementById('orderTable').innerHTML = table3 + '    <button type="button" onclick="read()">Go!</button>';;
+}
+function read() {
+  content = document.getElementsByClassName("content");
+  order = document.getElementsByClassName("order");
+  for (var i = 0; i < content.length; i++) {
+    link += "#//" + order[i].value + "#//" + content[i].value;
+  }
+  document.body.innerHTML += link;
+}
