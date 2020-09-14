0cbcc32f718fbe0b548d852c77b8811c02547649
United Geekdom <53636961+united-geekdom@users.noreply.github.com>
2020-09-14 12:47:17 +0530

    Update writer.js

--- games/jumbled-sentence/writer.js
+++ games/jumbled-sentence/writer.js
@@ -6,9 +6,9 @@ function tableContent() {
   link += "#//" + document.getElementById('row').value;
   link += "#//" + document.getElementById('collumn').value;
   var table1 = "";
-  for (var i = 0; i < document.getElementById('collumn').value; i++) {
+  for (var i = 0; i < document.getElementById('row').value; i++) {
     table1 += "<tr>";
-    for (var j = 0; j < document.getElementById('row').value; j++) {
+    for (var j = 0; j < document.getElementById('collumn').value; j++) {
       table1 += "<td><input value='' class='content'></td>";
     }
     table1 += "</tr>"
@@ -17,9 +17,9 @@ function tableContent() {
 }
 function tableOrder() {
   var table3 = "";
-  for (var i = 0; i < document.getElementById('collumn').value; i++) {
+  for (var i = 0; i < document.getElementById('row').value; i++) {
     table3 += "<tr>";
-    for (var j = 0; j < document.getElementById('row').value; j++) {
+    for (var j = 0; j < document.getElementById('collumn').value; j++) {
       table3 += "<td><input type='number' class='order'></td>";
     }
     table3 += "</tr>"
